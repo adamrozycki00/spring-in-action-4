09c81d65357ab177e5f11d04b2c8d4d78390b122
Adam Rozycki <adamrozycki00@gmail.com>
2020-10-23 21:07:46 +0200

    Enable Spring MVC (spittr)

--- spittr/src/main/java/spittr/config/WebConfig.java
+++ spittr/src/main/java/spittr/config/WebConfig.java
@@ -1,11 +1,17 @@
 package spittr.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+@Configuration
+@EnableWebMvc
+@ComponentScan("spitter.web")
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Bean
