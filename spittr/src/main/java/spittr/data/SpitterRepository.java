2d2d20e2c02a50a7d930f46dcbd6edca6b75855e
Adam Rozycki <adamrozycki00@gmail.com>
2020-11-09 08:33:17 +0100

    Add @Repository annotations

--- spittr/src/main/java/spittr/data/SpitterRepository.java
+++ spittr/src/main/java/spittr/data/SpitterRepository.java
@@ -1,7 +1,9 @@
 package spittr.data;
 
+import org.springframework.stereotype.Repository;
 import spittr.Spitter;
 
+@Repository
 public interface SpitterRepository {
 
     Spitter save(Spitter spitter);
--- spittr/src/main/java/spittr/data/SpittleRepository.java
+++ spittr/src/main/java/spittr/data/SpittleRepository.java
@@ -1,9 +1,11 @@
 package spittr.data;
 
+import org.springframework.stereotype.Repository;
 import spittr.Spittle;
 
 import java.util.List;
 
+@Repository
 public interface SpittleRepository {
 
     List<Spittle> findSpittles(long max, int count);
